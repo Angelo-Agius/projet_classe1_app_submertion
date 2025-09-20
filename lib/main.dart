1755642aeeb08d4eb2186e823fa9fd3aa291359d
AngeloAgius-dev <agiusangelo13@gmail.com>
2025-09-20 17:08:27 +0200

    [ * ] Edit app title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', home: MyHomePage());
+    return MaterialApp(title: 'App Submertion', home: MyHomePage());
   }
 }
 
